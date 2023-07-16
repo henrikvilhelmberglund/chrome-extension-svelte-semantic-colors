5ca7641d27c9d3581c3354e1766270ec372c0f46
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-07-16 13:55:12 +0200

    feat: :wrench: add adapter config

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from 'sveltekit-adapter-chrome-extension';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -6,7 +6,15 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false,
+			manifest: 'manifest.json'
+		}),
+		appDir: 'app'
 	}
 };
 
